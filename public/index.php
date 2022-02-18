7e82867136f0a612e1ef05a923eabad87da1fc5e
Asliddinbek Azizovich <asliddin.abdivasiyev@gmail.com>
2022-02-18 19:19:32 +0500

    fix: APP_DEBUG default value changed to false
    this commit belongs to ticket ASLIDDIN-S-PROJECT-T-1

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // $time_start = microtime(true);
 defined('APP_DIR') or define('APP_DIR', __DIR__ . '/../');
-defined('APP_DEBUG') or define('APP_DEBUG', true);
+defined('APP_DEBUG') or define('APP_DEBUG', false);
 
 require_once APP_DIR . '/app/core/bootstrap.php';
